70d215aa42827aa353906a8d1f1fdd1f06f959ea
Seeagarg <89650047+Seeagarg@users.noreply.github.com>
2023-10-24 17:19:35 +0530

    student Dashboard changed

---
+++ EduWave/src/pages/Student/Schedule/Schedule.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Schedule() {
+  return (
+    <div>
+      This is Schedule Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, quod aut. Eos, et minus? Eum, autem necessitatibus modi eius deleniti soluta fugit doloremque nesciunt, aperiam nisi architecto alias, explicabo numquam!
+    </div>
+  )
+}
+
+export default Schedule
---
+++ EduWave/src/pages/Student/Test/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div>
+      This is Test Module 
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis adipisci alias tempora provident nesciunt doloribus expedita ex quas quaerat nam deserunt temporibus voluptates fuga similique vitae molestias, dolorem, repudiandae itaque veritatis assumenda ad. A, inventore ratione tenetur dolores deleniti at quo repellat praesentium expedita? Sequi debitis magnam eos cum minima.\
+    </div>
+  )
+}
+
+export default Test
